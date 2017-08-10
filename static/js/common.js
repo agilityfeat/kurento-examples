507f077daabadae63a44c6aed6572efd66f01892
deviantfero <fmorataya.04@gmail.com>
2017-08-09 21:20:05 -0600

    add better handling of session types, conference, four-way and one-to-one

--- static/js/common.js
+++ static/js/common.js
@@ -1,14 +1,14 @@
 function connectSocket() {
   var a = document.createElement('a');
   a.href = window.location;
-  var url = `ws://${a.host}/ws${a.pathname}`;
+  var url = "ws://" + a.host + "/ws" + a.pathname;
   return new WebSocket(url);
 }
 
 function sendMessage(message) {
-	var jsonMessage = JSON.stringify(message);
-	console.log('SEND ', jsonMessage);
-	ws.send(jsonMessage);
+  var jsonMessage = JSON.stringify(message);
+  console.log('SEND ', jsonMessage);
+  ws.send(jsonMessage);
 }
 
 function onIceCandidate(candidate) {
@@ -23,13 +23,20 @@ function handleError(error) {
 }
 
 function createWebRTCPeer(options) {
-  webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options, function(error) {
+  var webRtcMode = options.mode || 'Sendrecv';
+  options.mode = undefined;
+  options.configuration = {
+    iceServers: [{"urls":"stun:stun.l.google.com:19302"}, {
+	"urls" : "turn:111.222.333.444", "username":"user", "credential":"pass"}]
+  };
+
+  webRtcPeer = kurentoUtils.WebRtcPeer['WebRtcPeer' + webRtcMode](options, function(error) {
     if(error) return handleError(error);
     this.generateOffer(function(error, sdpOffer) {
       if(error) return handleError(error);
       sendMessage({
-        action : 'offer',
-        offer : sdpOffer
+	action : 'offer',
+	offer : sdpOffer
       })
     });
   });
