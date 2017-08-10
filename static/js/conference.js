7987319395fc78d0942880bdb65030353942f456
deviantfero <fmorataya.04@gmail.com>
2017-08-09 21:21:29 -0600

    modify handlers for each session to centralize stun/turn configuration

--- static/js/conference.js
+++ static/js/conference.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(evt) {
   console.log('Creating WebRtcPeer and generating local sdp offer ...');
 
+  var type = window.location.pathname.split('/').pop();
+
   setTimeout(function () {
     createWebRTCPeer({
       localVideo: document.querySelector('#my-video'),
       remoteVideo: document.querySelector('#other-video'),
       onicecandidate : onIceCandidate,
+      type: type,
+      mode: type === 'presenter' ? 'Sendonly' : 'Recvonly'
     });
   }, 500);
 });
--- static/js/one-to-one.js
+++ static/js/one-to-one.js
@@ -6,10 +6,6 @@ document.addEventListener('DOMContentLoaded', function(evt) {
       localVideo: document.querySelector('#my-video'),
       remoteVideo: document.querySelector('#other-video'),
       onicecandidate : onIceCandidate,
-      configuration: {
-	iceServers: [{"urls":"stun:stun.l.google.com:19302"}, {
-	      "urls" : "turn:111.222.333.444", "username":"youruser", "credential":"yourpass"}]
-      }
     });
   }, 500);
 });
